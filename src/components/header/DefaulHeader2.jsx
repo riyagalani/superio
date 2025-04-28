041dea31effd64dfb583442ed799698afbf66388
riyagalani <riyagalani19@gmail.com>
2025-04-28 12:45:18 +0530

    first commit

---
+++ src/components/header/DefaulHeader2.jsx
@@ -0,0 +1,58 @@
+
+
+
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import HeaderNavContent from "./HeaderNavContent";
+
+
+const DefaulHeader2 = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 10) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeBackground);
+  }, []);
+
+  return (
+    // <!-- Main Header-->
+    <header
+      className={`main-header  ${
+        navbar ? "fixed-header animated slideInDown" : ""
+      }`}
+    >
+      {/* <!-- Main box --> */}
+      <div className="main-box">
+        {/* <!--Nav Outer --> */}
+        <div className="nav-outer">
+          <div className="logo-box">
+            <div className="logo">
+              <Link to="/">
+                <img
+                 
+                  src="/images/logo.svg"
+                  alt="brand"
+                />
+              </Link>
+            </div>
+          </div>
+          {/* End .logo-box */}
+
+          <HeaderNavContent />
+          {/* <!-- Main Menu End--> */}
+        </div>
+        {/* End .nav-outer */}
+        
+      </div>
+    </header>
+  );
+};
+
+export default DefaulHeader2;
